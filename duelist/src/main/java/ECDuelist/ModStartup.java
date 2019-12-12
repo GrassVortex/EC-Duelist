f8feaf198eadd79a5bd7bb000e2fdcd9d1ff14b4
GrassVortex <58793188+GrassVortex@users.noreply.github.com>
2019-12-12 20:12:33 +0100

    card settings
    Started on setting files for cards. Cards should be able to "inherit" settings to make some reuse easier.

--- duelist/src/main/java/ECDuelist/ModStartup.java
+++ duelist/src/main/java/ECDuelist/ModStartup.java
@@ -1,18 +1,34 @@
 package ECDuelist;
 
+import ECDuelist.Settings.CardLibrary;
+import basemod.interfaces.EditCardsSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 
 
 @SpireInitializer
-public class ModStartup {
+public class ModStartup implements
+		  EditCardsSubscriber {
 
-    public ModStartup() {
+	private CardLibrary library;
 
-        System.out.println("Hello StS World!");
-    }
+	public ModStartup() {
 
-    public static void initialize() {
-        ModStartup start = new ModStartup();
+		System.out.println("Hello StS World!");
 
-    }
+		library = new CardLibrary();
+	}
+
+	@SuppressWarnings("unused")
+	// This class is indirectly used by ModTheSpire. But the tools can't verify it, therefore the warning
+	public static void initialize() {
+		// Create an instance of out main mod class. Do all initialization in the constructor and related methods, not in this method.
+		ModStartup start = new ModStartup();
+	}
+
+
+	@Override
+	public void receiveEditCards() {
+		library.registerCards();
+
+	}
 }
---
+++ duelist/src/main/java/ECDuelist/Settings/CardLibrary.java
@@ -0,0 +1,40 @@
+package ECDuelist.Settings;
+
+import ECDuelist.Cards.Card;
+import basemod.BaseMod;
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class CardLibrary {
+
+	private ArrayList<Card> cards;
+
+	public CardLibrary() {
+		InputStream in = CardLibrary.class.getResourceAsStream("/settings/cardLibrary.json");
+		Gson reader = new Gson();
+		LibraryList list = reader.fromJson(new InputStreamReader(in, StandardCharsets.UTF_8), LibraryList.class);
+
+		cards = new ArrayList<Card>();
+		for (int i = 0; i < list.Cards.length; i++) {
+			cards.add(loadCard(list.Cards[i]));
+		}
+	}
+
+	private Card loadCard(String cardId) {
+		return new Card(cardId);
+	}
+
+	public void registerCards() {
+		for (int i = 0; i < cards.size(); i++) {
+			BaseMod.addCard(cards.get(i));
+		}
+	}
+
+	private class LibraryList {
+		public String[] Cards;
+	}
+}
