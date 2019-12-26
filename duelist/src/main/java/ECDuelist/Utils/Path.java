208a3b3ee3c201790ee3bc6fa66885909cd2dd8f
GrassVortex <58793188+GrassVortex@users.noreply.github.com>
2019-12-27 00:52:00 +0100

    Moved resources
    Moved all resources to a prefixed subfolder

--- duelist/src/main/java/ECDuelist/Utils/Path.java
+++ duelist/src/main/java/ECDuelist/Utils/Path.java
@@ -1,7 +1,7 @@
 package ECDuelist.Utils;
 
 public class Path {
-	public static final String ResourcesBasePath = "/";
+	public static final String ResourcesBasePath = "/ECDuelist-Resources/";
 	public static final String SettingsPath = ResourcesBasePath + "settings/";
 	public static final String ImagesPath = ResourcesBasePath + "images/";
 
