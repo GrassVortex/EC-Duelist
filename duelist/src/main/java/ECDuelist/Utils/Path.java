8ebd1852876c4014d0be87a31d53ae2454a68f10
GrassVortex <58793188+GrassVortex@users.noreply.github.com>
2019-12-27 19:49:38 +0100

    Update Path.java
    image resource loading also cares about the leading '/' it seems

--- duelist/src/main/java/ECDuelist/Utils/Path.java
+++ duelist/src/main/java/ECDuelist/Utils/Path.java
@@ -4,6 +4,6 @@ public class Path {
 	public static final String ResourcesBasePathNotAbsolute = "ECDuelistFiles/";
 	public static final String ResourcesBasePath = "/" + ResourcesBasePathNotAbsolute;
 	public static final String SettingsPath = ResourcesBasePath + "settings/";
-	public static final String ImagesPath = ResourcesBasePath + "images/";
+	public static final String ImagesPath = ResourcesBasePathNotAbsolute + "images/";
 
 }
